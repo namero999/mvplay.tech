88ee787d6b34cdf433e412e9952d01c66e4ebf4a
Corrado Alesso <co.alesso@fastwebnet.it>
2015-09-29 01:00:53 +0200

    A bit of funny mess

--- s3sync.sh
+++ s3sync.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-s3cmd sync --rr --acl-public --exclude-from .s3ignore --delete-removed ./ s3://mvplay.tech
+s3cmd sync --rr --acl-public --exclude-from .s3ignore --delete-removed --add-header='Cache-Control:max-age=30' ./ s3://mvplay.tech
